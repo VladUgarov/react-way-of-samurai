4c0810d9ba76d27b5e2a04ba78fb9166d8342cb6
VladUgarov <69145359+VladUgarov@users.noreply.github.com>
2020-12-25 16:52:07 +0300

    Finish 70

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,6 +10,7 @@ import {
 import Users from "./Users";
 import Preloader from "../Common/Preloader/Preloader";
 import { withAuthRedirect } from "../../hoc/WithAuthRedirect";
+import { compose } from "redux";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
@@ -51,12 +52,13 @@ let mapStateToProps = (state) => {
   };
 };
 
-export default withAuthRedirect(
+export default compose(
+  withAuthRedirect,
   connect(mapStateToProps, {
     follow,
     unfollow,
     setCurrentPage,
     toggleFollowingProgress,
     getUsers
-  })(UsersContainer)
-);
+  })
+)(UsersContainer);
